26b8ea8840a61dd8f7b8de0c010b7f91660e4b5d
Desiree Garcia <desireegarcia@Macintosh.hsd1.nm.comcast.net>
2021-10-14 17:45:59 -0600

    added files

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Form from './Form';
+import formSchema from './formSchema';
+import User from './User';
+import * as yup from 'yup';
+import './App.css';
+
+
+const initialFormValues = {
+  name: '',
+  email: '',
+  password: '',
+  terms: false
+}
+const initialFormErrors = {
+  name: '',
+  email: '',
+  password: '',
+}
+const initialUser = []
+const initialDisabled = true
+
+
+function App() {
+
+  const [user, setUser] = useState(initialUser)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+  const getUser = () => {
+    axios.get('https://reqres.in/api/users')
+    .then(res => {
+      setUser(res.data.data)
+    }).catch(err => console.error(err))
+  }
+
+  const postNewUser = newUser => {
+    axios.post('https://reqres.in/api/users', newUser)
+    .then(res => {
+      setUser([res.data, ...user])
+    }).catch(err => console.error(err)
+    .finally(() => {
+      setFormValues(initialFormValues);
+    }))
+  }
+
+  const validate = (name, value) => {
+    yup.reach(formSchema, name)
+    .validate(value)
+    .then(() => setFormErrors({ ...formErrors, [name]: ''}))
+    .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
+  }
+
+  const inputChange = (name, value) => {
+    validate(name, value);
+    setFormValues({
+      ...formValues, [name]: value
+    })
+  }
+
+    const formSubmit = () => {
+      const newUser = {
+        name: formValues.name.trim(),
+        email: formValues.email.trim(),
+        password: formValues.password.trim(),
+        terms: ['terms'].filter(terms => !!formValues[terms])
+      }
+      postNewUser(newUser);
+    }
+    useEffect(() => {
+      getUser()
+    }, [])
+
+    useEffect(() => {
+      formSchema.isValid(formValues).then(valid => setDisabled(!valid))
+    }, [formValues])
+
+  return (
+    <div className="container">
+      {/* <header className="App-header"> */}
+        <h2>User Form</h2>
+      {/* </header> */}
+      <Form 
+      values={formValues}
+      change={inputChange}
+      submit={formSubmit}
+      disabled={disabled}
+      errors={formErrors}
+      />
+      {/* {
+        user.map(users => {
+          return (
+            
+          )
+        })
+      } */}
+    </div>
+  );
+}
+
+export default App;
---
+++ user-onboarding/src/formSchema.js
@@ -0,0 +1,28 @@
+import * as yup from 'yup';
+
+const formSchema = yup.object().shape({
+   name: yup
+        .string()
+        .trim()
+        .required('Username is required!')
+        .min(3, 'Username must be 3 or more characters long!'),
+    email: yup
+        .string()
+        .email('Must be a valid email address!')
+        .required('Email is required!'),
+    password: yup
+        .string()
+        .required('Password is required!')
+        .matches(
+            /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
+            "Password must contain at least 8 characters, one uppercase, one number and one special case character"),
+    // confirmPassword: yup
+    //       .string()
+    //       .required("Please confirm your password")
+    //       .when("password", {
+    //         is: password => (password && password.length > 0 ? true : false),
+    //         then: yup.string().oneOf([yup.ref("password")], "Password doesn't match")
+    terms: yup.boolean()
+});
+
+export default formSchema;
